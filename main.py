9ea43eab055a2b8579add7ebc1ee61c813a9480f
Verduxo <87647482+Verduxo@users.noreply.github.com>
2021-07-19 13:40:45 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import rainbowtext 
+
+
+print(rainbowtext.text('\n\nhecho con <3 por Verduxo, con el apoyo de !Hacking Of The World'))
+print(rainbowtext.text('''
+
+  ._.  ___ ___  ________   ___________ __      __ 
+  | | /   |   \ \_____  \  \__    ___//  \    /  \
+      
+  | |/    ~    \ /   |   \   |    |   \   \/\/   /
+   \|\    Y    //    |    \  |    |    \        / 
+   __ \___|_  / \_______  /  |____|     \__/\  /  
+   \/       \/          \/                   \/   
+\n\n\n                
+'''))
